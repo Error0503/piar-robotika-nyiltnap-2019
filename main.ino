3e24a09fffbf643c6815470ea69887ab7051b9d6
Error0503 <43830867+Error0503@users.noreply.github.com>
2019-11-18 16:07:53 +0100

    UserInput added

--- main.ino
+++ main.ino
@@ -1,10 +1,28 @@
+#define 1button 1
+#define 2button 2
+#define 3button 3
+#define okButton 4
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() 
+  pinMode(1button, INPUT);
+  pinMode(2button, INPUT);
+  pinMode(3button, INPUT);
+  pinMode(okButton, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void UserInput() {
+  int selected = 0;
+  
+  do {
+    if (digitalRead(1button) ==  HIGH) {
+      //placeholder beacuse why not
+    }
+  } while (digitalRead(okButton) == HIGH);
+  
 }
