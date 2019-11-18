b7dc70a43a07d660f7b5265ec882f0e83333da5f
SiposMB <45514402+SiposMB@users.noreply.github.com>
2019-11-18 16:17:44 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,7 +2,21 @@
 #define 2button 2
 #define 3button 3
 #define okButton 4
+int boxes [3] = [4, 5, 5];
+bool is_player_turn = false;
+randomSeed(2);
 
+void AI (){
+  // nim összeg
+  int n = table[0] ^ table[1] ^ table[2];
+  // valid lépés keresése
+  if (n != 0){
+    for(int i = 0; 
+   for    
+  }else{
+    boxes[random(0,3)]--;
+  }
+}
 void setup() 
   pinMode(1button, INPUT);
   pinMode(2button, INPUT);
